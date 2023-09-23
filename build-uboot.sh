d33d53d16ce966172e542ccf3d0afd9a4adcb775
Karol Przybylski <karol.przybylski@consult.red>
2023-09-23 15:52:54 +0200

    Initial commit

---
+++ build-uboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+# shellcheck source=setup.sh
+. setup.sh
+
+git clone git://git.denx.de/u-boot.git
+
+git checkout v2023.07 -b v2023.07
+
+cd $UBOOT_DIR
+
+export PATH=$CROSSTOOL_DIR/x-tools/aarch64-rpi4-linux-gnu/bin/:$PATH
+
+export CROSS_COMPILE=aarch64-rpi4-linux-gnu-
+
+make rpi_4_defconfig
+
+make
